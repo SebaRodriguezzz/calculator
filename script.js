adad9703ce750431d79dd007660c75d7618797f0
Sebastian Rodriguez <sebas.r1070@gmail.com>
2023-02-25 15:17:03 -0300

    Add functionality to substract operator

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
     const divide = ((a, b) => parseInt(a) / parseInt(b));
 
-    const operate = ((operator, a, b) => {operator(a, b)});
+    const operate = ((operator, a, b) => operator(a, b));
 
     const numberButtons = document.querySelectorAll('.number');
     const numberDisplay = document.querySelector('.numbers');
@@ -25,10 +25,9 @@
     let secondNumber = '';
     let result = '0';
 
-    if (firstNumber === ''){
-        numberButtons.forEach(number => 
-            number.addEventListener('click', addListenerFirst));
-    };
+    numberButtons.forEach(number => 
+        number.addEventListener('click', addListenerFirst));
+
 
     function addListenerFirst(e) {
         firstNumber += e.target.textContent;
@@ -45,18 +44,51 @@
             number.removeEventListener('click', addListenerFirst));
     }
 
+    function removeListenerSecond(){
+        numberButtons.forEach(number => 
+            number.removeEventListener('click', addListenerSecond));
+    }
+
     plus.addEventListener('click', () => {
         removeListenerFirst();
         numberDisplay.textContent += ' + ';
-        if (secondNumber === ''){
-            numberButtons.forEach(number => 
-                number.addEventListener('click', addListenerSecond));
+        numberButtons.forEach(number => 
+            number.addEventListener('click', addListenerSecond));
+
+        if (secondNumber != '') {
+            result = (operate(add, firstNumber, secondNumber)).toString();
+            secondNumber = '';
+            firstNumber = result;
+            resultDisplay.textContent = result;
+        }
+        
+    });
+
+    minus.addEventListener('click', () => {
+        removeListenerFirst();
+        numberDisplay.textContent += ' - ';
+        numberButtons.forEach(number => 
+            number.addEventListener('click', addListenerSecond));
+
+        if (secondNumber != '') {
+            result = (operate(subtract, firstNumber, secondNumber)).toString();
+            secondNumber = '';
+            firstNumber = result;
+            resultDisplay.textContent = result;
         }
     });
 
     clear.addEventListener('click', () => {
+        removeListenerSecond();
         numberDisplay.textContent = '\u00A0';
         resultDisplay.textContent = '0';
         firstNumber = '';
         secondNumber = '';
+        numberButtons.forEach(number => 
+            number.addEventListener('click', addListenerFirst));
+    });
+
+    equals.addEventListener('click', () => {
+        // todo 
+        resultDisplay.textContent = result;
     });
