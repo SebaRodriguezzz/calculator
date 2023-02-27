d7c6ae01325a9fecc9bb6c6e97eb20c0a872a2c3
Sebastian Rodriguez <sebas.r1070@gmail.com>
2023-02-27 13:33:49 -0300

    Finish script

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const add = ((a, b) => parseFloat(a) + parseFloat(b));
-
-const subtract = ((a, b) => parseFloat(a) - parseFloat(b));
-
-const multiply = ((a, b) => parseFloat(a) * parseFloat(b));
-
-const divide = ((a, b) => parseFloat(a) / parseFloat(b));
-
-const operate = ((operator, a, b) => operator(a, b));
-
 const numberButtons = document.querySelectorAll('.number');
 const numberDisplay = document.querySelector('.numbers');
 const resultDisplay = document.querySelector('.result');
@@ -20,14 +10,141 @@ const multiplier = document.querySelector('.multiplier');
 const equals = document.querySelector('.equals');
 const point = document.querySelector('.point');
 
-
 let firstNumber = '';
 let secondNumber = '';
-let result = '0';
+let result = '';
 let firstOperation = true;
+let firstUsage = true;
+let firstNumberPoint = true;
+let secondNumberPoint = true;
+const percentageCalc = 100;
 
-numberButtons.forEach(number =>
-    number.addEventListener('click', addListenerFirst));
+const add = ((a, b) => parseFloat(a) + parseFloat(b));
+const subtract = ((a, b) => parseFloat(a) - parseFloat(b));
+const multiply = ((a, b) => parseFloat(a) * parseFloat(b));
+const divide = ((a, b) => parseFloat(a) / parseFloat(b));
+const percentage = ((a, b) => (parseFloat(b) * parseFloat(a)) / 100);
+const operate = ((operator, a, b) => operator(a, b));
+
+numberButtons.forEach(number => number.addEventListener('click', addListenerFirst));
+
+plus.addEventListener('click', () => {
+    if (firstNumber != '' && (firstOperation === true || secondNumber === '' || result != '')) {
+        removeListenerFirst();
+        handleFirstOperation();
+        handleConcatenated();
+        if (firstUsage === true || secondNumber != '') {
+            numberDisplay.textContent += ' + ';
+            firstUsage = false;
+        }
+        numberButtons.forEach(number => number.addEventListener('click', addListenerSecond));
+        equals.addEventListener('click', () => handleResult(add));
+        if (secondNumber != '') {
+            secondNumber = '';
+            firstNumber = result;
+        }
+        firstUsage = true;
+    }
+});
+
+minus.addEventListener('click', () => {
+    if (firstNumber != '' && (firstOperation === true || secondNumber === '' || result != '')) {
+        removeListenerFirst();
+        handleFirstOperation();
+        handleConcatenated();
+        if (firstUsage === true || secondNumber != '') {
+            numberDisplay.textContent += ' - ';
+            firstUsage = false;
+        }
+        numberButtons.forEach(number => number.addEventListener('click', addListenerSecond));
+        equals.addEventListener('click', () => handleResult(subtract));
+        if (secondNumber != '') {
+            secondNumber = '';
+            firstNumber = result;
+        }
+        firstUsage = true;
+    }
+});
+
+slash.addEventListener('click', () => {
+    if (firstNumber != '' && (firstOperation === true || secondNumber === '' || result != '')) {
+        removeListenerFirst();
+        handleFirstOperation();
+        handleConcatenated();
+        if (firstUsage === true || secondNumber != '') {
+            numberDisplay.textContent += ' / ';
+            firstUsage = false;
+        }
+        numberButtons.forEach(number => number.addEventListener('click', addListenerSecond));
+        equals.addEventListener('click', () => handleResult(divide));
+        if (secondNumber != '') {
+            secondNumber = '';
+            firstNumber = result;
+        }
+        firstUsage = true;
+    }
+});
+
+multiplier.addEventListener('click', () => {
+    if (firstNumber != '' && (firstOperation === true || secondNumber === '' || result != '')) {
+        removeListenerFirst();
+        handleFirstOperation();
+        handleConcatenated();
+        if (firstUsage === true || secondNumber != '') {
+            numberDisplay.textContent += ' * ';
+            firstUsage = false;
+        }
+        numberButtons.forEach(number => number.addEventListener('click', addListenerSecond));
+        equals.addEventListener('click', () => handleResult(multiply));
+        if (secondNumber != '') {
+            secondNumber = '';
+            firstNumber = result;
+        }
+        firstUsage = true;
+    }
+});
+
+percentile.addEventListener('click', () => {
+    if (firstNumber != '' && (firstOperation === true || secondNumber === '' || result != '')) {
+        removeListenerFirst();
+        handleFirstOperation();
+        handleConcatenated();
+        if (firstUsage === true || secondNumber != '') {
+            numberDisplay.textContent += ' % ';
+            firstUsage = false;
+        }
+        numberButtons.forEach(number => number.addEventListener('click', addListenerSecond));
+        equals.addEventListener('click', () => handleResult(percentage));
+        if (secondNumber != '') {
+            secondNumber = '';
+            firstNumber = result;
+        }
+        firstUsage = true;
+    }
+});
+
+
+point.addEventListener('click', () => {
+    if (firstNumber != '' && secondNumber === '' && firstNumberPoint === true) {
+        firstNumber += '.';
+        firstNumberPoint = false;
+        numberDisplay.textContent += '.';
+    } else if (secondNumber != '' && secondNumberPoint === true) {
+        secondNumber += '.';
+        secondNumberPoint = false;
+        numberDisplay.textContent += '.';
+    }
+})
+
+clear.addEventListener('click', () => {
+    removeListenerSecond();
+    numberDisplay.textContent = '\u00A0';
+    resultDisplay.textContent = '0';
+    firstNumber = '';
+    secondNumber = '';
+    result = '';
+    numberButtons.forEach(number => number.addEventListener('click', addListenerFirst));
+});
 
 function addListenerFirst(e) {
     firstNumber += e.target.textContent;
@@ -51,7 +168,7 @@ function removeListenerSecond() {
 
 function handleFirstOperation() {
     if (firstOperation === false) {
-        if (result != ''){
+        if (result != '') {
             firstNumber = result;
         }
     } else {
@@ -63,82 +180,17 @@ function handleResult(operator) {
     result = (operate(operator, firstNumber, secondNumber)).toString();
     resultDisplay.textContent = result;
     removeListenerSecond();
+    resetFirstUsages();
 }
 
-function handleConcatenated(){
+function handleConcatenated() {
     if (firstNumber === result) {
         numberDisplay.textContent = result;
     }
 }
 
-plus.addEventListener('click', () => {
-    removeListenerFirst();
-    handleFirstOperation();
-    handleConcatenated();
-    numberDisplay.textContent += ' + ';
-    numberButtons.forEach(number => number.addEventListener('click', addListenerSecond));
-    equals.addEventListener('click', () => handleResult(add));
-    if (secondNumber != '') {
-        secondNumber = '';
-        firstNumber = result;
-    }
-});
-
-minus.addEventListener('click', () => {
-    removeListenerFirst();
-    handleFirstOperation();
-    handleConcatenated();
-    numberDisplay.textContent += ' - ';
-    numberButtons.forEach(number => number.addEventListener('click', addListenerSecond));
-    equals.addEventListener('click', () => handleResult(subtract));
-    if (secondNumber != '') {
-        secondNumber = '';
-        firstNumber = result;
-    }
-});
-
-slash.addEventListener('click', () => {
-    removeListenerFirst();
-    handleFirstOperation();
-    handleConcatenated();
-    numberDisplay.textContent += ' / ';
-    numberButtons.forEach(number => number.addEventListener('click', addListenerSecond));
-    equals.addEventListener('click', () => handleResult(divide));
-    if (secondNumber != '') {
-        secondNumber = '';
-        firstNumber = result;
-    }
-});
-
-multiplier.addEventListener('click', () => {
-    removeListenerFirst();
-    handleFirstOperation();
-    handleConcatenated();
-    numberDisplay.textContent += ' * ';
-    numberButtons.forEach(number => number.addEventListener('click', addListenerSecond));
-    equals.addEventListener('click', () => handleResult(multiply));
-    if (secondNumber != '') {
-        secondNumber = '';
-        firstNumber = result;
-    }
-});
-
-point.addEventListener('click', () => {
-    numberDisplay.textContent += '.';
-    if (secondNumber === ''){
-        firstNumber += '.';
-    } else {
-        secondNumber += '.';
-    }
-})
-
-clear.addEventListener('click', () => {
-    removeListenerSecond();
-    numberDisplay.textContent = '\u00A0';
-    resultDisplay.textContent = '0';
-    firstNumber = '';
-    secondNumber = '';
-    result = '';
-    numberButtons.forEach(number =>
-        number.addEventListener('click', addListenerFirst));
-});
+function resetFirstUsages() {
+    firstUsage = true;
+    firstNumberPoint = true;
+    secondNumberPoint = true;
+}
